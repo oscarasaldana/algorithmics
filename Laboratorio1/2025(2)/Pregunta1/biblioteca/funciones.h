f6f52c435760bb844756d8bb2ce6434ee6c1a675
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:37:47 -0500

    Update funciones.h

--- Laboratorio1/2025(2)/Pregunta1/biblioteca/funciones.h
+++ Laboratorio1/2025(2)/Pregunta1/biblioteca/funciones.h
@@ -1,1 +1,11 @@
 
+#ifndef PREGUNTA1_FUNCIONES_H
+#define PREGUNTA1_FUNCIONES_H
+
+const int F = 8;
+const int C = 4;
+
+void solucion(int (*)[C], int , int );
+void generarNumero(int *, int , int , int );
+
+#endif //PREGUNTA1_FUNCIONES_H
