81f2d2dfc43ea8cea0b59d351a690cd06cbcd545
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:26:03 -0500

    Update main.cpp

--- Laboratorio1/2026(1)/Pregunta1/main.cpp
+++ Laboratorio1/2026(1)/Pregunta1/main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/recursion.h"
+
+int main() {
+
+    int area[fila][columna] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int contador = 0;
+    int i = 0;
+
+    recursion(area,5,5,3,i,contador);
+
+    cout << "El robot encontro " << contador << " artefactos." << endl;
+
+    return 0;
+}
