b29b4ee8f45d0b30cc46cd64d2cfc8a2abfe1c6c
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:33:55 -0500

    Update funciones.h

--- Laboratorio1/2026(1)/Pregunta2/biblioteca/funciones.h
+++ Laboratorio1/2026(1)/Pregunta2/biblioteca/funciones.h
@@ -1,1 +1,8 @@
 
+#ifndef PREGUNTA2_FUNCIONES_H
+#define PREGUNTA2_FUNCIONES_H
+
+void solucion(int (*)[4], int (*)[3], int *, int , int );
+void generarCombinacion(int *, int , int , int );
+
+#endif //PREGUNTA2_FUNCIONES_H
