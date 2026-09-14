5165ae63ba37e4a39387e4517e73adcd4da1bc90
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:28:35 -0500

    Update recursion.h

--- Laboratorio1/2026(1)/Pregunta1/biblioteca/recursion.h
+++ Laboratorio1/2026(1)/Pregunta1/biblioteca/recursion.h
@@ -1,1 +1,10 @@
 
+#ifndef PREGUNTA1_RECURSION_H
+#define PREGUNTA1_RECURSION_H
+
+const int fila = 10;
+const int columna = 10;
+
+void recursion(int (*)[columna], int , int , int , int , int &);
+
+#endif //PREGUNTA1_RECURSION_H
