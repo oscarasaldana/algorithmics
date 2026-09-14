946cb94d2fadc236add9126e6ff7e808a33f81ce
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:28:13 -0500

    Update recursion.cpp
    Implement recursion function to count and clear specific elements in a 2D array.

--- Laboratorio1/2026(1)/Pregunta1/biblioteca/recursion.cpp
+++ Laboratorio1/2026(1)/Pregunta1/biblioteca/recursion.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+
+#include "recursion.h"
+
+void recursion(int (*area)[columna], int posFila, int posColumna, int limite, int i, int &contador) {
+
+    int filaEspejo;
+
+    if (limite < 0) return;
+  
+    filaEspejo = posFila;
+    if (area[posFila - i][posColumna - i] == 1) {
+        contador++;
+        area[posFila - i][posColumna - i] = 0;
+    }
+    if (area[posFila - i][posColumna + i] == 1) {
+        contador++;
+        area[posFila - i][posColumna + i] = 0;
+    }
+    if (area[filaEspejo + i][posColumna - i] == 1) {
+        contador++;
+        area[filaEspejo + i][posColumna - i] = 0;
+    }
+    if (area[filaEspejo + i][posColumna + i] == 1) {
+        contador++;
+        area[filaEspejo + i][posColumna + i] = 0;
+    }
+    if (i == limite) {
+        i = -1;
+        posFila--;
+        filaEspejo++;
+        limite--;
+    }
+    recursion(area,posFila,posColumna,limite,i + 1,contador);
+
+}
