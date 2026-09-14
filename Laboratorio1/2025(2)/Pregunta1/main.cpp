3c447a3fd05ead0c1cc5eb5ce61fc6aad79f4aba
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 12:37:05 -0500

    Update main.cpp

--- Laboratorio1/2025(2)/Pregunta1/main.cpp
+++ Laboratorio1/2025(2)/Pregunta1/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    int informacionControladores[F][C] = {
+        {1, 35000, 80, 1},
+        {2, 24000, 60, 3},
+        {3, 30000, 70, 2},
+        {4, 27000, 48, 1},
+        {5, 10000, 20, 1},
+        {6,  7000, 35, 2},
+        {7,  6000, 10, 1},
+        {8, 40000, 40, 3}
+    };
+
+    int base = 2;
+    int numComponentes = 8;
+
+    solucion(informacionControladores,base,numComponentes);
+
+    return 0;
+}
