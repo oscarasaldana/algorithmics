5100276a3c4879e363ce592be201b07ca3878ce9
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 22:32:39 -0500

    Update funciones.h

--- Laboratorio1/2025(1)/Pregunta2/item(b)/biblioteca/funciones.h
+++ Laboratorio1/2025(1)/Pregunta2/item(b)/biblioteca/funciones.h
@@ -1,1 +1,8 @@
 
+#ifndef ITEM_B__FUNCIONES_H
+#define ITEM_B__FUNCIONES_H
+
+void generarNumero(int *, int , int , int );
+void solucion(int (*)[3], int (*)[3], int , int );
+
+#endif //ITEM_B__FUNCIONES_H
