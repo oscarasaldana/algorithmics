45b8bcb79dc876a8b1fa7c32a45aed78225b229b
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 22:22:16 -0500

    Update funciones.h

--- Laboratorio1/2025(1)/Pregunta1/biblioteca/funciones.h
+++ Laboratorio1/2025(1)/Pregunta1/biblioteca/funciones.h
@@ -1,1 +1,9 @@
 
+#ifndef PREGUNTA1_FUNCIONES_H
+#define PREGUNTA1_FUNCIONES_H
+
+void solucion(int *, int *, int , int );
+void generarNumero(int *, int , int , int );
+int encontrarEspacioMinimo(int *, int &);
+
+#endif //PREGUNTA1_FUNCIONES_H
