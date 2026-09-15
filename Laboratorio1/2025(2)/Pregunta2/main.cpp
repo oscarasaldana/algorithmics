2c27edd1881bc03a26c4898a43c6dfa1100b8938
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 22:18:42 -0500

    Update main.cpp

--- Laboratorio1/2025(2)/Pregunta2/main.cpp
+++ Laboratorio1/2025(2)/Pregunta2/main.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+#include "biblioteca/recursion.h"
+
+int main() {
+
+    int mina[fila][columna] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    //Piedras
+    mina[5][5] = -1;
+    mina[4][5] = -1;
+    mina[3][5] = -1;
+
+    mostrarMina(mina);
+    recursion(mina,5,0,1);
+    cout << endl;
+    mostrarCaminos(mina);
+
+}
