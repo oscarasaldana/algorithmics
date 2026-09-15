5667a6f4917cc70b10d1c6f008d1b4e0ba7f8958
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 22:18:16 -0500

    Update recursion.h

--- Laboratorio1/2025(2)/Pregunta2/biblioteca/recursion.h
+++ Laboratorio1/2025(2)/Pregunta2/biblioteca/recursion.h
@@ -1,1 +1,12 @@
 
+#ifndef PREGUNTA2_RECURSION_H
+#define PREGUNTA2_RECURSION_H
+
+const int fila = 6;
+const int columna = 11;
+
+void mostrarMina(int (*)[columna]);
+bool recursion(int (*)[columna], int , int , int );
+void mostrarCaminos(int (*)[columna]);
+
+#endif //PREGUNTA2_RECURSION_H
