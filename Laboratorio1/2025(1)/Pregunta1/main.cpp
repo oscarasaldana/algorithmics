0cd9227fd640ff3ffd13784d0ac1cf02821e2546
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-14 22:22:30 -0500

    Update main.cpp

--- Laboratorio1/2025(1)/Pregunta1/main.cpp
+++ Laboratorio1/2025(1)/Pregunta1/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#include "biblioteca/funciones.h"
+
+int main() {
+
+    int tablas[6] = {150, 100, 80, 50, 120, 10};
+    int discos[3] = {250, 200, 200};
+
+    solucion(tablas,discos,3,6);
+
+    return 0;
+}
 
